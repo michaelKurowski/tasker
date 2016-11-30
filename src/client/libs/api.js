86b9bead427e7bb6c0e506c783fcb81760b3374f
Michael Kurowski <Byamarro@gmail.com>
2016-11-30 15:45:10 +0100

    Building in tasker panel into index.html
    new asstes:
     	modified:   src/client/assets/background.svg
     	new file:   src/client/assets/delete.png
     	new file:   src/client/assets/delete.xcf
    	new file:   src/client/assets/edit.png
     	new file:   src/client/assets/edit.xcf
    
    Added taskerPanel
     	modified:   src/client/index.html
    
    Added drafts of task and historyRecord prototypes. Created createTask() (WIP)
     	modified:   src/client/libs/api.js
    
    Cosmetic changes:
     	modified:   src/client/libs/webUtils.js
    
    Temp file:
     	modified:   src/client/taskerPanel.html
    
    Created section for tasker panel
     	modified:   src/client/theme.css

--- src/client/libs/api.js
+++ src/client/libs/api.js
@@ -1,4 +1,36 @@
-function login() {
+function api(){
+	screens = {}
+	screens.login = document.getElementById('loginPanel')
+	screens.tasker = document.getElementById('taskerPanel')
 
-	
+}
+function login() {
+	moveToScreen(screens.login, screens.tasker)
+}
+
+
+function createTask() {
+
+}
+
+var task = {
+	title: '',
+	urgency: 0,
+	importance: 0,
+	deadline: 0,
+	history: [],
+	addHistoryRecord: function (name){
+		var newHistoryRecord = Object.create(historyRecord)
+		newHistoryRecord.title = name
+		newHistoryRecord.timestamp = new Date().getTime()
+		this.history.push(newHistoryRecord)
+	},
+	delete: function () {
+
+	}
+}
+
+var historyRecord = {
+	title: '',
+	timestamp: 0
 }
--- src/client/libs/webUtils.js
+++ src/client/libs/webUtils.js
@@ -7,7 +7,7 @@ window.onload = function () {
 	toArray(inputsHostObj).forEach( function (element) {
 		declareInputField(element)
 	})
-
+	api()
 }
 
 
