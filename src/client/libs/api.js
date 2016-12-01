968826f0c3163ef4077710340e85f040063f1812
Michael Kurowski <Byamarro@gmail.com>
2016-12-01 19:42:43 +0100

    Better visuals. Tasks adding, removing and getting.
    New asset:
     	new file:   src/client/assets/add.png
    
    Visuals:
     	modified:   src/client/index.html
    
    added taskManager object with .list .addTask() .deleteTask() .getTasksByCategory() .renderTasks(). Finished task prototype
     	modified:   src/client/libs/api.js
    
    CSS changes:
     	modified:   src/client/theme.css

--- src/client/libs/api.js
+++ src/client/libs/api.js
@@ -1,16 +1,66 @@
+var tasksManager = {
+	list: [],
+	addTask: function (title, urgency, importance, deadline) {
+		this.list.push( createTask(title, urgency, importance, deadline) )
+	},//tasksManager.addTask("Dupa", 1, 1)
+	deleteTask: function(id) {
+		delete this.list[id]
+	},
+	getTasksByCategory: function (category) {
+		return this.list.filter( function (element) {
+			var alertCategory =
+				(element.urgency === 1 && element.importance === 1) ? 1 :
+				(element.urgency === 2 && element.importance === 1) ? 2 :
+				(element.urgency === 1 && element.importance === 2) ? 3 :
+				4
+			return alertCategory === category
+		})
+	},
+	renderTasks: function (tasksCategory) {
+		var html = ''
+		var listOfTasks = this.getTasksByCategory(tasksCategory)
+		listOfTasks.forEach( function (task) {
+			html += '<div class="alertWrapper"><div class="alert">'
+			html += task.title
+			html += '<div class="alertButtons"><span>'
+			html += '2h'
+			html += '</span><img src="assets/edit.png" width="15px" height="15px"></img><img src="assets/delete.png" width="15px" height="15px"></img></div></div>'
+			html += '<div class="history flex column">'
+			task.history.forEach( function (historyRecord) {
+				html += '<div class="historyRecord">'
+				html += historyRecord.title
+				html += '<div class="alertButtons"><img src="assets/edit.png" width="15px" height="15px"></img><img src="assets/delete.png" width="15px" height="15px"></img></div>'
+			})
+			html += '</div>'
+
+		})
+		return html
+	}
+}
+
+
 function api(){
 	screens = {}
 	screens.login = document.getElementById('loginPanel')
 	screens.tasker = document.getElementById('taskerPanel')
+
+
 
 }
 function login() {
 	moveToScreen(screens.login, screens.tasker)
 }
 
-function createTask() {
-
+function createTask(title, urgency, importance, deadline) {
+	deadline = deadline || 0
+	var newTask = Object.create(task)
+	newTask.title = title
+	newTask.urgency = urgency
+	newTask.importance = importance
+	newTask.deadline = deadline
+	return newTask
 }
+
 
 var task = {
 	title: '',
@@ -23,9 +73,6 @@ var task = {
 		newHistoryRecord.title = name
 		newHistoryRecord.timestamp = new Date().getTime()
 		this.history.push(newHistoryRecord)
-	},
-	delete: function () {
-
 	}
 }
 
