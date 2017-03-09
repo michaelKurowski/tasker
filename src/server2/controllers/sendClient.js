12c3f7d6ff7487eb17e65138099aa5016f3a7264
Michael Kurowski <Byamarro@gmail.com>
2017-03-09 08:38:18 +0100

    [controller] sendClient cleaned

--- src/server2/controllers/sendClient.js
+++ src/server2/controllers/sendClient.js
@@ -1,6 +1,4 @@
-let express = require('express')
 let path = require('path')
-let log = console.log
 module.exports = (req, res) => {
 	let accessedFile = req.originalUrl.slice(req.route.path.length - 1)
 	accessedFile = accessedFile || 'index.html'
