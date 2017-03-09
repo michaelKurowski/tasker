289c53e6a0c01c51dbb33fa2c1660ec1dae2b388
Michael Kurowski <Byamarro@gmail.com>
2017-03-09 07:58:51 +0100

    [controller] sendClient hotfix

--- src/server2/controllers/sendClient.js
+++ src/server2/controllers/sendClient.js
@@ -4,8 +4,9 @@ let log = console.log
 module.exports = (req, res) => {
 	let accessedFile = req.originalUrl.slice(req.route.path.length - 1)
 	accessedFile = accessedFile || 'index.html'
-	res.sendfile(
-		// "../" is consider malicious, path.resolve is neccessary to avoid troubles
-		path.resolve(`../client/${accessedFile}`)
+	res.sendFile(
+		// "../" is consider dangerous in some cases,
+		// path.resolve is neccessary to avoid restrictions
+		`${path.resolve('../client')}/${accessedFile}`
 	)
 }
