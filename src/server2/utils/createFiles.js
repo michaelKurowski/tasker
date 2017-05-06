5a3e823e2f453d8d208e3f232ee39be28e8b4e53
Michael Kurowski <Byamarro@gmail.com>
2017-05-06 04:10:40 +0200

    [server] utils/createFiles() data sanitization

--- src/server2/utils/createFiles.js
+++ src/server2/utils/createFiles.js
@@ -1,11 +1,25 @@
 const fs = require('fs')
 const chalk = require('chalk')
 const log = console.log
+const path = require('path')
+//creates multiple .js files
 module.exports = (
 	/*
 	Array of objects that contains data about files to generate.
 	This function will iterate through all objects and create files based on
 	fileNameProperty argument.
+	example:
+		[
+			{
+				myFileName: 'file01',
+				otherThing: 4
+			},
+			{
+				myFileName: 'file02',
+				otherThing: 1
+			}
+		]
+		myFileName property is taken from fileNameProperty argument of this function
 	*/
 	listOfObjects,
 	//Directory to output files
@@ -13,10 +27,17 @@ module.exports = (
 	//Property in config file that defined files names
 	fileNameProperty,
 	//Default string that will be outputted to file
-	defaultContent
+	defaultContent,
+	//Log creation of new files to console
+	logToConsole = true
 	) => {
 	let filesCreationPromises = []
-	if ( !fs.existsSync(destinationPath) ) fs.mkdirSync(destinationPath);
+	if ( !fs.existsSync(destinationPath) ) fs.mkdirSync(destinationPath)
+	if (listOfObjects.length === 0)
+		return Promise.reject('List of files to create is empty.')
+	let correctCheck = listOfObjects.filter(elem => elem[fileNameProperty] !== undefined)
+	if (correctCheck.length !== listOfObjects.length)
+		return Promise.reject('At least one element from list of files to create, doesn\'t contains specified name property.')
 	listOfObjects.forEach( (element, index) => {
 		let outputPath = `${destinationPath}/${element[fileNameProperty]}.js`
 		if ( !fs.existsSync(outputPath) ) {
@@ -30,7 +51,7 @@ module.exports = (
 								reject(err)
 								log(chalk.red(err))
 							} else {
-								log('Created file: ', outputPath)
+								if (logToConsole) log('Created file: ', outputPath)
 								resolve()
 							}
 						}
